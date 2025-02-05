b5d8ece3306b81652d5f3962975779877123cfe1
Tomas Castro <tomasecastro@gmail.com>
2025-02-05 13:08:20 +0100

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,26 +2,49 @@
 
 set -e
 
-# set the postgres database host, port, user and password according to the environment
-# and pass them as arguments to the odoo process if not present in the config file
+# Configuración de la base de datos
 : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
 : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
 : ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo17@2023'}}}
 
-# install python packages
-pip3 install pip --upgrade
+# Instalar paquetes de Python necesarios
+pip3 install --upgrade pip
 pip3 install -r /etc/odoo/requirements.txt
 
-# sed -i 's|raise werkzeug.exceptions.BadRequest(msg)|self.jsonrequest = {}|g' /usr/lib/python3/dist-packages/odoo/http.py
+# Montar el bucket de MinIO utilizando s3fs
+if [ -z "$MINIO_ACCESS_KEY" ] || [ -z "$MINIO_SECRET_KEY" ] || [ -z "$MINIO_URL" ] || [ -z "$MINIO_BUCKET" ]; then
+    echo "Las variables de entorno MINIO_ACCESS_KEY, MINIO_SECRET_KEY, MINIO_URL y MINIO_BUCKET deben estar definidas."
+    exit 1
+fi
 
+# Crear el archivo de contraseñas para s3fs
+echo "$MINIO_ACCESS_KEY:$MINIO_SECRET_KEY" > /etc/odoo/.passwd-s3fs
+chmod 600 /etc/odoo/.passwd-s3fs
+
+# Crear el punto de montaje si no existe
+#mkdir -p /mnt/minio
+chown odoo:odoo /mnt/minio
+
+# Montar el bucket de MinIO
+#s3fs $MINIO_BUCKET /mnt/minio -o passwd_file=/root/.passwd-s3fs -o url=$MINIO_URL -o use_path_request_style -o allow_other
+
+# Verificar si el montaje fue exitoso
+#if mountpoint -q /mnt/minio; then
+#    echo "El bucket de MinIO se montó correctamente en /mnt/minio."
+#else
+#    echo "Error al montar el bucket de MinIO."
+#    exit 1
+#fi
+
+# Configuración de Odoo
 DB_ARGS=()
 function check_config() {
     param="$1"
     value="$2"
-    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
-        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
-    fi;
+    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then
+        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" | cut -d " " -f3 | sed 's/["\n\r]//g')
+    fi
     DB_ARGS+=("--${param}")
     DB_ARGS+=("${value}")
 }
--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 DESTINATION=$1
 PORT=$2
 CHAT=$3
+MINIO_PATH=$DESTINATION/odoo-data
+# Obtener el nombre de usuario y grupo actuales
+USER=$(whoami)
+GROUP=$(id -gn $USER)
 
 # Clonar el directorio de Odoo
 git clone --depth=1 https://github.com/tomasecastro/odoo-17-docker-compose $DESTINATION
@@ -15,6 +19,9 @@ mkdir -p $DESTINATION/postgresql
 sudo chown -R $USER:$USER $DESTINATION
 sudo chmod -R 700 $DESTINATION  # Solo el usuario tiene acceso
 
+
+
+
 # Generar claves de Minio dinámicamente si no están definidas en el archivo .env
 if ! grep -q "^POSTGRES_PASSWORD=" $DESTINATION/.env; then
   export POSTGRES_PASSWORD=$(openssl rand -base64 12)  # Generar una clave de acceso aleatoria
@@ -24,12 +31,14 @@ else
   sed -i "s#^POSTGRES_PASSWORD=.*#POSTGRES_PASSWORD=$(openssl rand -base64 12)#" $DESTINATION/.env
 fi
 
+
 if ! grep -q "^MINIO_ROOT_USER=" $DESTINATION/.env; then
   export MINIO_ROOT_USER=$(openssl rand -base64 12)  # Generar una clave de acceso aleatoria
   echo "MINIO_ROOT_USER=$MINIO_ROOT_USER" >> $DESTINATION/.env
 else
   # Si ya existe, actualizar el valor
-  sed -i "s#^MINIO_ROOT_USER=.*#MINIO_ROOT_USER=$(openssl rand -base64 12)#" $DESTINATION/.env
+  export MINIO_ROOT_USER=$(openssl rand -base64 12)  # Generar una clave de acceso aleatoria
+  sed -i "s#^MINIO_ROOT_USER=.*#MINIO_ROOT_USER=$MINIO_ROOT_USER#" $DESTINATION/.env
 fi
 
 if ! grep -q "^MINIO_ROOT_PASSWORD=" $DESTINATION/.env; then
@@ -37,7 +46,8 @@ if ! grep -q "^MINIO_ROOT_PASSWORD=" $DESTINATION/.env; then
   echo "MINIO_ROOT_PASSWORD=$MINIO_ROOT_PASSWORD" >> $DESTINATION/.env
 else
   # Si ya existe, actualizar el valor
-  sed -i "s#^MINIO_ROOT_PASSWORD=.*#MINIO_ROOT_PASSWORD=$(openssl rand -base64 16)#" $DESTINATION/.env
+  export MINIO_ROOT_PASSWORD=$(openssl rand -base64 16)  # Generar una contraseña aleatoria
+  sed -i "s#^MINIO_ROOT_PASSWORD=.*#MINIO_ROOT_PASSWORD=$MINIO_ROOT_PASSWORD#" $DESTINATION/.env
 fi
 
 if ! grep -q "^MINIO_ACCESS_KEY=" $DESTINATION/.env; then
@@ -45,7 +55,8 @@ if ! grep -q "^MINIO_ACCESS_KEY=" $DESTINATION/.env; then
   echo "MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY" >> $DESTINATION/.env
 else
   # Si ya existe, actualizar el valor
-  sed -i "s#^MINIO_ACCESS_KEY=.*#MINIO_ACCESS_KEY=$(openssl rand -base64 12)#" $DESTINATION/.env
+  export MINIO_ACCESS_KEY=$(openssl rand -base64 12)  # Generar una clave de acceso aleatoria
+  sed -i "s#^MINIO_ACCESS_KEY=.*#MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY#" $DESTINATION/.env
 fi
 
 if ! grep -q "^MINIO_SECRET_KEY=" $DESTINATION/.env; then
@@ -53,7 +64,8 @@ if ! grep -q "^MINIO_SECRET_KEY=" $DESTINATION/.env; then
   echo "MINIO_SECRET_KEY=$MINIO_SECRET_KEY" >> $DESTINATION/.env
 else
   # Si ya existe, actualizar el valor
-  sed -i "s#^MINIO_SECRET_KEY=.*#MINIO_SECRET_KEY=$(openssl rand -base64 16)#" $DESTINATION/.env
+  export MINIO_SECRET_KEY=$(openssl rand -base64 16)  # Generar una contraseña aleatoria
+  sed -i "s#^MINIO_SECRET_KEY=.*#MINIO_SECRET_KEY=$MINIO_SECRET_KEY#" $DESTINATION/.env
 fi
 
 # Actualizar las variables ODOO_PORT y ODOO_LONGPOLLING_PORT en el archivo .env
@@ -73,6 +85,7 @@ else
   sed -i "s#^ODOO_LONGPOLLING_PORT=.*#ODOO_LONGPOLLING_PORT=$CHAT#" $DESTINATION/.env
 fi
 
+
 # Establecer permisos de archivos y directorios después de la instalación
 find $DESTINATION -type f -exec chmod 644 {} \;
 find $DESTINATION -type d -exec chmod 755 {} \;
@@ -81,11 +94,88 @@ find $DESTINATION -type d -exec chmod 755 {} \;
 chmod -R 777 $DESTINATION/addons $DESTINATION/etc $DESTINATION/postgresql
 
 # Ejecutar Odoo
-#docker-compose -f $DESTINATION/docker-compose.yml up -d
+docker-compose -f $DESTINATION/docker-compose.yml up -d minio
+
+# Esperar hasta que MinIO esté completamente operativo
+echo "Esperando a que MinIO esté disponible..."
+while ! curl -s http://localhost:9000/minio/health/live >/dev/null; do
+    echo "MinIO aún no está listo. Esperando..."
+    sleep 5
+done
+echo "MinIO está listo."
+
+# Descargar y configurar el cliente mc si no existe
+if ! command -v mc &> /dev/null; then
+    echo "Descargando MinIO Client (mc)..."
+    wget -q https://dl.min.io/client/mc/release/linux-amd64/mc -O /usr/local/bin/mc
+    chmod +x /usr/local/bin/mc
+fi
+
+# Configurar MinIO Client
+mc alias set myminio http://localhost:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+
+# Crear bucket si no existe
+if ! mc ls myminio | grep -q "odoo-bucket"; then
+    echo "Creando bucket 'odoo-bucket'..."
+    mc mb myminio/odoo-bucket
+    mc anonymous set private myminio/odoo-bucket
+fi
+
+# Crear credenciales adicionales si es necesario
+if ! mc admin user list myminio | grep -q "odoo-user"; then
+    echo "Creando usuario 'odoo-user'..."
+    mc admin user add myminio odoo-user $MINIO_SECRET_KEY
+    echo "Asignar privilegios usuario 'odoo-user'..."
+    mc admin policy attach myminio readwrite --user odoo-user
+    echo "Crear access key usuario 'odoo-user'..."
+    mc admin accesskey create myminio/ odoo-user --access-key $MINIO_ACCESS_KEY --secret-key $MINIO_SECRET_KEY
+fi
+
+
 
 # Obtener la dirección IP local
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 
+mkdir $MINIO_PATH
+chmod -R 777 $MINIO_PATH
+echo "$MINIO_ACCESS_KEY:$MINIO_SECRET_KEY" >> $DESTINATION/.passwd-s3fs
+chmod 600 $DESTINATION/.passwd-s3fs
+apt-get update && apt-get install -y s3fs
+s3fs odoo-bucket $MINIO_PATH -o dbglevel=info -f -o curldbg -o passwd_file=$DESTINATION/.passwd-s3fs -o host=http://$IP_ADDRESS:9000 -o endpoint=us-east-1 -o use_path_request_style -o allow_other &
+
+# Definir la ruta completa del directorio de Minio
+MINIO_DIR="$BASE_DIR/$MINIO_PATH"  # Esto asume que odoo está dentro de la ruta actual
+
+# Crear el archivo de servicio systemd para s3fs
+cat <<EOF | sudo tee /etc/systemd/system/s3fs-odoo-bucket.service
+[Unit]
+Description=Montar el bucket S3 odoo-bucket usando s3fs
+After=network.target
+
+[Service]
+ExecStartPre=/bin/bash -c "until docker ps | grep -q 'minio'; do echo 'Esperando a que el contenedor minio esté arriba...'; sleep 5; done"
+ExecStart=/usr/bin/s3fs odoo-bucket $MINIO_DIR -o passwd_file=$BASE_DIR/$DESTINATION/.passwd-s3fs -o host=http://$IP_ADDRESS:9000 -o endpoint=us-east-1 -o use_path_request_style -o allow_other
+Restart=always
+User=$USER
+Group=$GROUP
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Recargar systemd para reconocer el nuevo servicio
+sudo systemctl daemon-reload
+
+# Habilitar el servicio para que se inicie automáticamente al arrancar el sistema
+sudo systemctl enable s3fs-odoo-bucket.service
+
+# Iniciar el servicio
+sudo systemctl start s3fs-odoo-bucket.service
+
+# Ejecutar Odoo
+docker-compose -f $DESTINATION/docker-compose.yml up -d
+
 # Mostrar información de acceso
 echo "Odoo iniciado en http://$IP_ADDRESS:$PORT | Contraseña maestra: minhng.info | Puerto de chat en vivo: $CHAT"
-echo "El minIOiniciado en http://$IP_ADDRESS:9001 | Usuario por defecto: admin, y la contraseña maestra: $MINIO_ROOT_PASSWORD"
+echo "El minIOiniciado en http://$IP_ADDRESS:9001 | Usuario por defecto: $MINIO_ROOT_USER, y la contraseña maestra: $MINIO_ROOT_PASSWORD"
+echo "Se creo el servicio /etc/systemd/system/s3fs-odoo-bucket.service para controlar el s3fs"
