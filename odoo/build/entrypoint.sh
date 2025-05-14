7b6d298d432a46c32a17e25eba8e8051ba0b877b
xwifimasterx <juanjosebricenocol@gmail.com>
2025-05-14 11:46:18 -0500

    Update entrypoint.sh

--- odoo/build/entrypoint.sh
+++ odoo/build/entrypoint.sh
@@ -2,25 +2,39 @@
 
 set -e
 
-# Configuración de la base de datos
+# set the postgres database host, port, user and password according to the environment
+# and pass them as arguments to the odoo process if not present in the config file
 : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
 : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo17@2023'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo18@2024'}}}
 
-# Instalar paquetes de Python necesarios
-pip3 install --upgrade pip
-pip3 install -r /etc/odoo/requirements.txt
-pip3 install geopy dropbox pyncclient nextcloud-api-wrapper boto3 paramiko minio
+# install python packages
+# pip3 install pip --upgrade                # may cause errors
+#pip3 install -r /etc/odoo/requirements.txt
+pip install geopy dropbox pyncclient nextcloud-api-wrapper boto3 paramiko minio --break-system-packages
 
-# Configuración de Odoo
+
+# sed -i 's|raise werkzeug.exceptions.BadRequest(msg)|self.jsonrequest = {}|g' /usr/lib/python3/dist-packages/odoo/http.py
+
+# Install logrotate if not already installed
+if ! dpkg -l | grep -q logrotate; then
+    apt-get update && apt-get install -y logrotate
+fi
+
+# Copy logrotate config
+cp /etc/odoo/logrotate /etc/logrotate.d/odoo
+
+# Start cron daemon (required for logrotate)
+cron
+
 DB_ARGS=()
 function check_config() {
     param="$1"
     value="$2"
     if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then
-        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" | cut -d " " -f3 | sed 's/["\n\r]//g')
-    fi
+        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
+    fi;
     DB_ARGS+=("--${param}")
     DB_ARGS+=("${value}")
 }
